36e87217a690f089b592ed2605be91a7b4f51ae4
Matthew Dee <Matthew@matthews-macbook-pro.local>
2016-02-21 18:21:20 +0000

    results view controller added

---
+++ ResultsVC.swift
@@ -0,0 +1,16 @@
+//
+//  ResultsVC.swift
+//  Depression1
+//
+//  Created by Matthew Dee on 21/02/2016.
+//  Copyright © 2016 Matthew Dee. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ResultsVC {
+    
+    
+    
+}
