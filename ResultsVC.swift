76dbca6d8bc08ea69937cc6b410a4d3ec094214a
Matthew Dee <Matthew@matthews-macbook-pro.local>
2016-02-21 18:31:18 +0000

    IBOulet added and class created for results

--- ResultsVC.swift
+++ ResultsVC.swift
@@ -9,7 +9,22 @@
 import UIKit
 import CoreData
 
-class ResultsVC {
+
+class ResultsVC: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     
     
     
